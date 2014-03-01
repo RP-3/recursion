17a4e557394c8e5abe90f4c285c49859efdaac7f
Rohan <sarith21@gmail.com>
2014-03-01 15:14:57 +1100

    Stringify complete non-recursively
    Wrote a non-recursive version of stringily as a starting point. Does
    not consider all scenarios and edge cases (notably, strings within
    strings). Produces the same output as JSON.stringify in when given the
    input:
    
    var myObj = {
    	1: "the loneliest number",
    	Two: ["a drink with jam and bread", "a dear", 52, false]
    };
    
    Also does not take a replacer function or a spacing argument. Will
    first attempt to implement this beginning function recursively before
    incorporating those.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,37 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+var output = [];
+
+var stringifyObject = function(object){
+	if (Object.prototype.toString.apply(object) === '[object Array]'){
+		var output = []
+		for (var i=0; i<object.length; i++){
+			output.push(stringify(object[i]));
+		}
+		return "[" + output + "]";
+	}
+	else{return object;} //ignoring objects within objects
+}
+
+//function that stringifies differently dependent on value
+var stringify = function(stringMe){ 
+	switch(typeof stringMe){
+		case 'string': return '"' + stringMe + '"' ;
+		case 'object': return stringifyObject(stringMe); //defined above
+		default: return stringMe;
+	}
+}
+
+
+  for (key in obj){
+  	output.push('"'+key+'"' + ":" + stringify(obj[key]) );
+  }
+  return "{" + output.toString() + "}";
+};
+
+
+var myObj = {
+	1: "the loneliest number",
+	Two: ["a drink with jam and bread", "a dear", 52, false]
 };
