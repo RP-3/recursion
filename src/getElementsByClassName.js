ae85a2fcdf525c5dee2c2ba152591dc878755218
Rohan <sarith21@gmail.com>
2014-03-09 16:00:41 +1100

    Created an HTML file in which to create/tweak getElByClNm function
    New HTML file created.
    
    Put up what I thought would be the scaffolding of the function but on
    reflection, seems to do most of the job.
    
    Testing function exists called compareResults, that conLogs, pairwise,
    the correct results and this new function’s results.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,31 @@
 // };
 
 // But in stead we're going to implement it from scratch:
-var getElementsByClassName = function (className) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+ 	var results = [];	
+
+  	var fetchElement = function(node){
+  		if(node.className === className){
+  			results.push(node.outerHTML);
+  		}
+  	}
+
+  	var traverseChildren = function(node){
+		var childArray = node.childNodes;
+		//iterate through all children
+		for (var i=0; i<childArray.length; i++){
+			//if children, recurse
+			if(childArray[i].childNodes.length > 0){ 
+				traverseChildren(childArray[i]);
+			}
+			//if match, fetch element
+			fetchElement(childArray[i]); 
+		}
+	}
+
+	traverseChildren(document.body);
+
+	return results;
+
 };
